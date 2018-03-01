66e38bf643490c46ab2af15b4a966ce0067a00ae
daniel <dpillay03@gmail.com>
2018-02-28 18:54:47 -0800

    body parser code added and my port is running on 3000

--- lab-daniel/model/resource-api.js
+++ lab-daniel/model/resource-api.js
@@ -3,3 +3,15 @@
 const uuid = require('uuid/v4');
 
 
+class Fighter {
+    constructor(name, wins, losses) {
+        // the id is generated when a new Fighter is created.
+        // the id is NOT supplied by the user.
+        this.id = uuid();
+        this.name = name;
+        this.wins = wins;
+        this.losses = losses;
+    }
+}
+
+module.exports = Fighter;
--- lab-daniel/server.js
+++ lab-daniel/server.js
@@ -1,3 +1,13 @@
 'use strict';
-
 const http = require('http');
+const resource = require('./model/resource-api.js');
+const bodyParse = require('./lib/body-parser.js');
+const PORT = process.ENV || 3000;
+const server = http.createServer(function (req, res) {
+    req.url = url.parse(req.url);
+    req.url.query = querystring.parse(req.url.query);
+});
+
+server.listen(PORT, (re) => {
+    console.log(`http://localhost:${PORT}`);
+});
