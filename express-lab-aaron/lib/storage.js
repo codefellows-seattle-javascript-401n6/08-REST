3fd05d427d0df274e44b2e7cd00b8d0f25735919
AaronBruce <aaron.bruce@comcast.net>
2018-03-18 13:28:14 -0700

    got getAll and getOne functionale to work

--- express-lab-aaron/lib/storage.js
+++ express-lab-aaron/lib/storage.js
@@ -7,28 +7,35 @@ let thePound = {};
 
 const dog = () => {
   
-  thePound = {};
+  // thePound = [];
   
-  const michael = new Dog('Michael', 3, 'St-Bernard');
-  const jb = new Dog('JB', 6, 'beagle');
-  const steve = new Dog('Steve', 5, 'poodle');
+  const michael = new Dog('michael', 3, 'St-Bernard');
+  const jb = new Dog('jb', 6, 'beagle');
+  const steve = new Dog('steve', 5, 'poodle');
 
-  thePound[michael.id] = michael;
-  thePound[jb.id] = jb;
-  thePound[steve.id] = steve;
+  // thePound[michael.id] = michael;
+  // thePound[jb.id] = jb;
+  // thePound[steve.id] = steve;
+  thePound[michael.name] = michael;
+  thePound[jb.name] = jb;
+  thePound[steve.name] = steve;
+  // thePound.push(michael, jb, steve);
+  console.log('KEYS', Object.keys(thePound));
 };
 
 const getAll = () => {
+  dog();
+  // console.log(Object.keys(thePound));
   console.log('storge.js-POUND: ', thePound);
-  // return Object.values(thePound);
   return Object.values(thePound);
 };
 
-const getDog = (id) => {
-  if (!id in thePound) {
-    throw 'Dog not found in thePound with ID of: ' + id;
+const getDog = (name) => {
+  if (!name in thePound) {
+    console.log(`Dog not found in thePound with ID of: ${name}`);
   };
-  return thePound[id];
+  return thePound[name];
 };
 
 module.exports = {getDog, getAll, dog};
+// module.exports = {getAll, dog};
