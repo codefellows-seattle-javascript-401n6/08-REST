7900642f9ee1cec75297457414042a77bb051053
AaronBruce <aaron.bruce@comcast.net>
2018-03-18 19:06:05 -0700

    added delete and post functionale, but they dont work yet

--- express-lab-aaron/tests/test.js
+++ express-lab-aaron/tests/test.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const server = require('./server.js');
+const Router = express.Router();
+const storage = require('../lib/storage.js');
+
+describe('reader function', () => {
+  it('should return a dog object of all file values', (resolve, reject) => {
+    // let result = reader(paths, callback);
+    let result = reader(paths, callback);
+    let expected = []; //not really expecting an empty array
+    // let expected = 'this is a message from aaron I am testing this code to see if this file text is mapped last in the array';
+    // expect(result).toEqual(expected);
+    expect(result).toBe(expected);
+    resolve();
+  });
+});
